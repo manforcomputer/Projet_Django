3ced2ef6ae495c5ff8dc8b69926c40c0ce2914fa
manforcomputer <iphonemarius4@gmail.com>
2026-04-09 00:23:10 +0300

    Correction settings.py: ajout import os et STATIC_ROOT

--- pizzamama/pizzamama/settings.py
+++ pizzamama/pizzamama/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-(9%z#+gh9w94s&deyq3%+lqg4d11&l#n6=bi$lyfq0f28&^6#p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["guymarius.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
