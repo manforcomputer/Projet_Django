6546a6e904d605cdcbb3e7f335fa375142953344
manforcomputer <iphonemarius4@gmail.com>
2026-04-09 01:51:22 +0300

    Cache SECRET_KEY avec .env

--- pizzamama/pizzamama/settings.py
+++ pizzamama/pizzamama/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(9%z#+gh9w94s&deyq3%+lqg4d11&l#n6=bi$lyfq0f28&^6#p"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
